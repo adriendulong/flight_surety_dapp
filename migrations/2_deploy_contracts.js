61baa034f8a16ff5fd61980ae9c9f3cc8d5b70c8
Adrien Dulong <adrien.dulong@gmail.com>
2019-04-26 11:46:54 +0200

    Deploy first commit
    Init the project with truffle boilerplate and add the smart contracts
    plus tests that are needed.

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,23 @@
+const FlightSuretyApp = artifacts.require("FlightSuretyApp");
+const FlightSuretyData = artifacts.require("FlightSuretyData");
+const fs = require('fs');
+
+module.exports = function(deployer) {
+
+  let firstAirline = '0xeF03573F1E1ea59DFD36802018968ca063f1a9b0';
+  deployer.deploy(FlightSuretyData)
+  .then(() => {
+    return deployer.deploy(FlightSuretyApp)
+      .then(() => {
+        // let config = {
+        //   localhost: {
+        //     url: 'http://localhost:8545',
+        //     dataAddress: FlightSuretyData.address,
+        //     appAddress: FlightSuretyApp.address
+        //   }
+        // }
+        // fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+        // fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+      });
+  });
+}
